8c474d85004d893c780d2105789bbbaf068c89f5
Ayanokozii <ayanokozi0925@gmail.com>
2024-07-23 18:15:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import json
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext
 
 # Load configuration
 with open('config.json', 'r') as f:
@@ -15,41 +15,32 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Define the start command handler
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(config['quizBot']['messages']['welcome'])
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text(config['quizBot']['messages']['welcome'])
 
 # Define the error handler
-def error(update: Update, context: CallbackContext) -> None:
+async def error(update: Update, context: CallbackContext) -> None:
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     if update.message:
-        update.message.reply_text(config['quizBot']['messages']['error'])
+        await update.message.reply_text(config['quizBot']['messages']['error'])
     else:
         logger.error('No message found in update.')
 
 # Main function to start the bot
 def main() -> None:
-    # Create the Updater and pass it your bot's token
+    # Create the Application and pass it your bot's token
     # Make sure to replace 'YOUR_TOKEN_HERE' with your actual token
-    with open('config.json', 'r') as f:
-        config = json.load(f)
     token = config.get('token', '7332008423:AAFExbt7RhYJZ9IhR8lFQ4IQZVYvXYiIkYs')
-    updater = Updater(token)
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(token).build()
 
     # Register the start command handler
-    dispatcher.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
     # Register the error handler
-    dispatcher.add_error_handler(error)
+    application.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
